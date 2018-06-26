97f03e44b03d9ffe0a0c46884eb8a48a5e64dbe5
krivera1015 <krivera1015@gmail.com>
2018-06-26 23:49:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ function updateObjectWithKeyAndValue(obj, ingredients, amount) {
 }
 
 function destructiveUpdateObjectWithKeyAndValue(obj, ingredients, amount) {
-  return obj[ingredients] = amount;
+  obj[ingredients] = amount;
+  return obj;
 }
